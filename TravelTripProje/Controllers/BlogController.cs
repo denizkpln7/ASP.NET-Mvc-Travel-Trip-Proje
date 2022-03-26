77df1d5279f0adbafda5a75f386631caf9f9336d
denizkpln7 <denizkaplan580@gmail.com>
2022-03-26 19:13:08 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+
+        BlogYorum by = new BlogYorum();
+        public ActionResult BlogDetay(int id)
+        {
+            //var blogbul = c.Blogs.Where(x =>x.ID == id).ToList();
+            by.Deger1= c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+        }
+
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar b)
+        {
+            c.Yorumlars.Add(b);
+            c.SaveChanges();
+            return PartialView();
+
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).ToList();
+            return PartialView(degerler);
+        }
+    }
+}
